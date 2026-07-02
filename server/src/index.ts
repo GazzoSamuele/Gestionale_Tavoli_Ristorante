e47281f8a824e5d71da922fc96c406cd3337843a
GazzoSamuele <samuelegazzo69@gmail.com>
2026-07-02 18:19:44 +0200

    aggiunta del db mongoose, aggiunta scehma del singolo tavolo e lo schema di tutti i tavoli

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,28 @@
-import express from 'express'
+import 'dotenv/config'
+
+import Tavolo from './models/Tavolo'
+import express from 'express';
+import mongoose from 'mongoose';    
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+  throw new Error('MONGODB_URI manca nel file .env')
+}
+
+console.log('DEBUG inizio URI:', JSON.stringify(MONGODB_URI.slice(0, 18)))
+
+mongoose.connect(MONGODB_URI)
+  .then(async () => {
+    console.log('Connesso a MongoDB ✅')
+    const nuovoTavolo = await Tavolo.create({ numero: 1, posti: 4 })
+
+    // Tavolo.create salva un tavolo nel DB; è un'operazione lenta 
+    // (viaggio fino al cloud), e await dice "aspetta che finisca prima di continuare"
+
+    console.log('Tavolo creato:', nuovoTavolo)
+  })
+  .catch((err) => console.error('Errore connessione MongoDB:', err))
 
 const app = express();
 const PORT = 3000;
---
+++ server/src/models/Tavolo.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+// dichiara la forma. La I davanti è una convenzione (sta per Interface)
+
+interface ITavolo {
+  numero: number;
+  posti: number;
+  stato: 'libero' | 'occupato' | 'riservato';
+  oraArrivo?: Date; 
+  
+}
+
+const tavoloSchema = new mongoose.Schema<ITavolo>({
+    numero: { type: Number, required: true, unique: true },
+    posti: { type: Number, required: true },
+    stato: {
+        type: String,
+        enum: ['libero', 'occupato', 'riservato'],
+        default: 'libero',
+        required: true
+    },
+    oraArrivo: { type: Date }
+});
+
+const Tavolo = mongoose.model<ITavolo>('Tavolo', tavoloSchema);
+
+export default Tavolo;
